6f7687edb055d64088b3a9fda05d2f78263ef38e
Ethan Stark <ethanstark@gmail.com>
2017-02-10 17:01:56 -0800

    Testing routes

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import Trossello from './Trossello'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-});
+it('renders homepage without crashing', () => {
+  const div = document.createElement('div')
+  ReactDOM.render(<App />, div)
+})
+
+it('renders Trossello page without crashing', () => {
+  const div = document.createElement('div')
+  ReactDOM.render(<Trossello />, div)
+})
